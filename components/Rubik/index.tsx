d4e94291eef32e4c755fcfb9c15826bb92e6abd5
Ali Syahidin <ali.syahidin@sg-dsa.com>
2022-01-05 11:36:12 +0700

    prepare scene

---
+++ components/Rubik/index.tsx
@@ -0,0 +1,5 @@
+const Rubik = () => {
+  return null
+}
+
+export default Rubik
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    }
   },
   plugins: [],
 }
