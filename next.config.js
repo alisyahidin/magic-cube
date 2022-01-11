eac488b7e67743a34bddc1ebbedacc0cca121a74
Ali Syahidin <ali.syahidin@sg-dsa.com>
2022-01-11 13:47:35 +0700

    update http header for sitemap.xml

--- next.config.js
+++ next.config.js
@@ -5,4 +5,17 @@ module.exports = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          }
+        ],
+      }
+    ]
+  },
 }
